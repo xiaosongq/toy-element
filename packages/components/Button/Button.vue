edf83e0592799d46b7ae10ff63e7a4f7c83ccdce
ICE <11657082+xiaosongq@user.noreply.gitee.com>
2024-05-22 01:04:40 +0800

    :tada: first commit

---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  defineOptions({
+    name: "ErButton",
+  });
+</script>
+<template>
+  <button style="background-color: blue; color: red">this is a button</button>
+</template>
+
+
+<style scoped>
+
+</style> 
